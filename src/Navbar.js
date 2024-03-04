ff635f8d7a7e4c2def8c0f0416548d12226dacb3
mcasian <garrisonjr78@gmail.com>
2024-03-03 22:26:12 -0500

    Updated React project

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,15 @@
 function Navbar() {
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="https://www.example.com">Navbar</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="#">Home</a>
-          <a class="nav-item nav-link" href="#">About</a>
-          <a class="nav-item nav-link" href="#">Contact</a>
+          <a class="nav-item nav-link active" href="https://www.example.com">Home</a>
+          <a class="nav-item nav-link" href="https://www.example.com">About</a>
+          <a class="nav-item nav-link" href="https://www.example.com">Contact</a>
         </div>
       </div>
     </nav>
